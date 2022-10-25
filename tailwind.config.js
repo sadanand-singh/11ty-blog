2e9758fcf8bdd9aeb9235a73c7e834dd980c35a8
Sadanand Singh <sadanand4singh@gmail.com>
2022-10-25 11:58:20 +0530

    simplify css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-const { theme } = require('tailwindcss/defaultConfig')
+const { theme } = require('tailwindcss/defaultConfig');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ["./**/*.html"],
@@ -8,57 +9,6 @@ module.exports = {
       center: true,
     },
     extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            color: "text-gray-600",
-            a: {
-              color: "text-gray-600",
-              "&:hover": {
-                color: "text-indigo-500",
-              },
-            },
-            h1: {
-              color: "text-gray-700",
-            },
-            h2: {
-              color: "text-indigo-500",
-            },
-            h3: {
-              color: "text-indigo-500",
-            },
-            h4: {
-              color: "text-indigo-500",
-            },
-            blockquote: {
-              color: "text-gray-600",
-            },
-            strong: {
-              color: "text-gray-600",
-            },
-            blockquote: {
-              borderLeftWidth: "0.25rem",
-              borderLeftColor: "text-indigo-500",
-            },
-            "blockquote p:first-of-type::before": {
-              content: "",
-            },
-            "blockquote p:last-of-type::after": {
-              content: "",
-            },
-            code: {
-              color: "text-gray-600",
-              fontWeight: "500",
-            },
-            "code::before": {
-              content: "",
-            },
-            "code::after": {
-              content: "",
-            },
-          },
-        },
-      },
       margin: {
         "-72": "-18rem",
         "-80": "-20rem",
