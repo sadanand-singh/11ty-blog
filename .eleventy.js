ccac3746cc273ff166f104b5db5ab23e224fdcff
Sadanand Singh <sadanand4singh@gmail.com>
2022-10-26 15:16:06 +0530

    Add callouts

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ const pluginTOC = require('@thedigitalman/eleventy-plugin-toc-a11y');
 const site = require('./src/_data/site');
 const shortcodes = require('./lib/shortcodes/shortcodes.js')
 const pairedshortcodes = require('./lib/shortcodes/paired_shortcodes.js')
+const markdownLib = require('./lib/plugins/markdown');
 
 /**
  * Prefixes the given URL with the site's base URL.
@@ -17,39 +18,9 @@ const toAbsoluteUrl = (url) => {
   return new URL(url, site.baseUrl).href;
 }
 
-/* Markdown Plugins */
-let markdownIt = require("markdown-it");
-let markdownItAnchor = require("markdown-it-anchor");
-let markdownItEmoji = require("markdown-it-emoji");
-let markdownItFootnote = require("markdown-it-footnote");
-let markdownItContainer = require("markdown-it-container");
-let markdownItTasks = require('markdown-it-task-lists')
-let markdownItAttrs = require("markdown-it-attrs")
 
-const anchorSlugify = (s) =>
-  encodeURIComponent(
-    String(s)
-        .trim()
-        .toLowerCase()
-        .replace(/[.,\/#!$%\^&\*;:{}=_`~()]/g, '')
-        .replace(/\s+/g, '-')
-  );
 
-let options = {
-  html: true,
-  breaks: false,
-  linkify: true,
-  typographer: true
-};
 
-let opts = {
-  permalink: true,
-  permalinkSymbol: '',
-  permalinkClass: 'heading-anchor',
-  permalinkBefore: true,
-  level: 2,
-  slugify: anchorSlugify,
-};
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -87,25 +58,7 @@ module.exports = function (eleventyConfig) {
     )
   })
 
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
-    .use(markdownItEmoji)
-    .use(markdownItFootnote)
-    .use(markdownItContainer, 'callout')
-    .use(markdownItContainer, 'callout-blue')
-    .use(markdownItContainer, 'callout-pink')
-    .use(markdownItContainer, 'callout-green')
-    .use(markdownItContainer, 'warning')
-    .use(markdownItTasks)
-    .use(require('@iktakahiro/markdown-it-katex'), {
-      "throwOnError" : false,
-      "errorColor" : " #cc0000"
-    })
-    .use(markdownItAttrs, {
-      includeLevel: [2,3],
-      listType: "ol"
-    })
-  );
+  eleventyConfig.setLibrary('md', markdownLib);
 
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
