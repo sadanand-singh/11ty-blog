200f754a3bd72f5a727ef66a54ec2e9c8b6b51dc
Sadanand Singh <sadanand4singh@gmail.com>
2022-10-29 12:25:50 +0530

    add google tag manager tracking

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,18 @@
 const isDev = process.env.ELEVENTY_ENV === 'development';
+const isProd = process.env.ELEVENTY_ENV === 'production';
 
 const baseUrl = isDev ? `http://localhost:8080` : `https://reckoning.dev/`;
+const tag = process.env.GOOGLE_TAGMANAGER_ID
 
 const site = {
   title: 'reckoning.dev',
   author: 'Sadanand Singh',
   lang: 'en-US',
   comments_repo: 'sadanand-singh/reckoning.dev.comments',
+  google_tagmanager_id: tag,
   description: 'Sadanand Singh Blog and Digital Garden',
   baseUrl,
+  isProd
 }
 
 module.exports = site;
